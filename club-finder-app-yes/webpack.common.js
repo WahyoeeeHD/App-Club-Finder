9f611fa63805c0b5ba27afb8d767ca3248992149
WahyoeeeHD <wahyuupunya@gmail.com>
2025-03-25 22:04:00 +0800

    Update webpack.common.js
    Mengubah dist menjadi public agar dapat dipublish ke dalam vercel

--- club-finder-app-yes/webpack.common.js
+++ club-finder-app-yes/webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: path.join(__dirname, 'src/app.js'),
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
     },
     module: {
