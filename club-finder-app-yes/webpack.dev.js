9dbe02ac7be2842c528e4d1dda357fe24ebcfefc
WahyoeeeHD <wahyuupunya@gmail.com>
2025-03-25 22:04:44 +0800

    Update webpack.dev.js
    Mengubah dist menjadi public agar vercel bisa tau dimana tempat outputnya

--- club-finder-app-yes/webpack.dev.js
+++ club-finder-app-yes/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     mode: 'development',
     devServer: {
         static: {
-            directory: path.join(__dirname, 'dist'),
+            directory: path.join(__dirname, 'public'),
         },
         watchFiles: ['index.html', 'src/**/*'],
         open: true,
